a43624ca2e135d23c58f9aefc99f12e04f855c39
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-19 21:28:06 +0530

    feat: integrate src/main.ts

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// Entry point of the application
+import { bootstrap } from '@angular/platform-browser';
+import { provide } from '@angular/core';
+import { AppComponent } from './app/app.component';
+bootstrap(AppComponent, []);
